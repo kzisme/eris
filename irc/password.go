ec084f49ab14f088e7b5605881b027c5519812e6
James Mills <prologic@shortcircuit.net.au>
2017-11-16 00:30:31 -0800

    Removed docopt cruft and simplify main

--- irc/password.go
+++ irc/password.go
@@ -1,27 +1,14 @@
 package irc
 
 import (
-	"golang.org/x/crypto/bcrypt"
 	"encoding/base64"
 	"errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
 	EmptyPasswordError = errors.New("empty password")
 )
-
-func GenerateEncodedPassword(passwd string) (encoded string, err error) {
-	if passwd == "" {
-		err = EmptyPasswordError
-		return
-	}
-	bcrypted, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.MinCost)
-	if err != nil {
-		return
-	}
-	encoded = base64.StdEncoding.EncodeToString(bcrypted)
-	return
-}
 
 func DecodePassword(encoded string) (decoded []byte, err error) {
 	if encoded == "" {
--- main.go
+++ main.go
@@ -1,63 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"syscall"
+	"os"
 
-	"github.com/docopt/docopt-go"
-	"github.com/prologic/eris/irc"
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/prologic/eris/irc"
 )
 
 func main() {
-	version := irc.FullVersion()
-	usage := `eris.
-Usage:
-	eris genpasswd [--conf <filename>]
-	eris run [--conf <filename>] [ -d | --debug ]
-	eris -h | --help
-	eris -v | --version
-Options:
-	-c --conf <filename>  Configuration file to use [default: ircd.yml].
-	-h --help          Show this screen.
-	-v --version          Show version.`
+	var (
+		version    bool
+		debug      bool
+		configfile string
+	)
 
-	arguments, _ := docopt.Parse(usage, nil, true, version, false)
+	flag.BoolVar(&version, "v", false, "display version information")
+	flag.BoolVar(&debug, "d", false, "enable debug logging")
+	flag.StringVar(&configfile, "c", "ircd.yml", "config file")
+	flag.Parse()
 
-	if arguments["-d"].(bool) || arguments["--debug"].(bool) {
+	if version {
+		fmt.Printf("eris v%s", irc.FullVersion())
+		os.Exit(0)
+	}
+
+	if debug {
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.WarnLevel)
 	}
 
-	// Special case -- We do not need to load the config file here
-	if arguments["genpasswd"].(bool) {
-		fmt.Print("Enter Password: ")
-		bytePassword, err := terminal.ReadPassword(syscall.Stdin)
-		if err != nil {
-			log.Fatal("Error reading password:", err.Error())
-		}
-		password := string(bytePassword)
-		encoded, err := irc.GenerateEncodedPassword(password)
-		if err != nil {
-			log.Fatalln("encoding error:", err)
-		}
-		fmt.Print("\n")
-		fmt.Println(encoded)
-		return
-	}
-
-	configfile := arguments["--conf"].(string)
 	config, err := irc.LoadConfig(configfile)
 	if err != nil {
 		log.Fatal("Config file did not load successfully:", err.Error())
 	}
 
-	if arguments["run"].(bool) {
-		server := irc.NewServer(config)
-		log.Println(irc.FullVersion(), "running")
-		defer log.Println(irc.FullVersion(), "exiting")
-		server.Run()
-	}
+	irc.NewServer(config).Run()
 }
